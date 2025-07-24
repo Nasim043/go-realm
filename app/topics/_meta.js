3414f02398790102517d4a94d37681318eab540f
Nasim <nasimcse16@gmail.com>
2025-07-24 23:11:54 +0600

    enums

--- app/topics/_meta.js
+++ app/topics/_meta.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+  enum: "Enum",
   function: "Function",
   interface: "Interface",
   "interface-composition": "Interface Composition",
