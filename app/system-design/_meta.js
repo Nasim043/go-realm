ac6e8fcd2ca96bd7545102af34705d2929c031bc
nasim <nasimcse16@gmail.com>
2025-09-08 22:03:43 +0600

    api

---
+++ app/system-design/_meta.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    "api-design": "API Design",
+    "content-delivery-network": "Content Delivery Network",
+    "draft-roadmap": "Draft Roadmap",
+  };
