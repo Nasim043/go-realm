bf22847d839cd566039126a4efdff58f65ec6b29
nasim043 <nasimcse16@gmail.com>
2025-10-22 17:49:12 +0600

    add database learning content with examples and explanations

---
+++ app/database/_meta.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  "keys": "Keys",
+  "joins": "Join Types",
+  "relations": "Relations",
+  "crud": "CRUD",
+  "transactions": "Transactions",
+  "indexing": "Indexing",
+  "acid": "ACID",
+  "normalization": "Normalization",
+  "sql-vs-nosql": "SQL vs NoSQL",
+  "data-modeling": "Data Modeling",
+  "lock": "Locking",
+};
