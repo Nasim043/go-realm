62783b89ce3a09795133cbbe219877c149db3a25
nasim <nasimcse16@gmail.com>
2026-01-07 23:16:15 +0600

    Update Go routines documentation with comprehensive Channel guide and formatting improvements

--- app/go-routines/_meta.js
+++ app/go-routines/_meta.js
@@ -2,7 +2,7 @@
 export default {
   "concurrency-vs-parallelism": "Concurrency vs Parallelism",
   "day-1": "🧭 Day 1",
-  "day-2": "🧭 Day 2",
+  "day-2": "Go Channel",
   "day-3": "🧭 Day 3",
   "day-4": "🧭 Day 4",
   "day-5": "🧭 Day 5",
