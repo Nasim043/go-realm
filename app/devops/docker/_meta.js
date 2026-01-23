6821707c3cdbee9512009dea06425941aa2546cb
nasim <nasimcse16@gmail.com>
2026-01-23 17:46:59 +0600

    Add Linux process model and container fundamentals to Docker Day 2

--- app/devops/docker/_meta.js
+++ app/devops/docker/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
     "basic": "Basic",
+    "linux": "Linux & Process Model",
     "image-container": "Image & Container",
     "docker-compose": "Compose",
 };
